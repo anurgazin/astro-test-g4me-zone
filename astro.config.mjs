7eddef434d213e03a1ffe2c5a9546d52b89b43e2
anurgazin <dindog24@gmail.com>
2024-05-06 13:25:04 -0400

    vercel support added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  image: { domains: ["https://firebasestorage.googleapis.com/"] },
+  image: {
+    domains: ["https://firebasestorage.googleapis.com/"],
+  },
+  adapter: vercel(),
 });
