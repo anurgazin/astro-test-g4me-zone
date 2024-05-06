a8f29cca41b6e46052014f70840ecf835aa33c9c
anurgazin <dindog24@gmail.com>
2024-05-06 14:34:34 -0400

    Index page style changed, Cards style changed

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_id__b_qrDUzP.mjs
@@ -0,0 +1,5 @@
+export { renderers } from "../renderers.mjs";
+
+const page = () => import("./pages/_id__3aF5TTj0.mjs").then((n) => n._);
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_AAzSF4gw.mjs
@@ -0,0 +1,5 @@
+export { renderers } from "../renderers.mjs";
+
+const page = () => import("./prerender_D-NANbzi.mjs");
+
+export { page };
